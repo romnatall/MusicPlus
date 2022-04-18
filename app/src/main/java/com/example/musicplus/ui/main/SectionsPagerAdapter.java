d64b93e64147b42a130ac86214c821b7084fd208
Роман Наталичев <natalichev.ra@edu.spbstu.ru>
2022-04-18 11:05:38 +0300

    изменил scrollbar

--- app/src/main/java/com/example/musicplus/ui/main/SectionsPagerAdapter.java
+++ app/src/main/java/com/example/musicplus/ui/main/SectionsPagerAdapter.java
@@ -17,7 +17,7 @@ import com.example.musicplus.R;
 public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
     @StringRes
-    private static final int[] TAB_TITLES = new int[]{R.string.tab_text_1, R.string.tab_text_2};
+    private static final int[] TAB_TITLES = new int[]{R.string.tab_text_1, R.string.tab_text_2, R.string.tab_text_3, R.string.tab_text_4, R.string.tab_text_5, R.string.tab_text_6};
     private final Context mContext;
 
     public SectionsPagerAdapter(Context context, FragmentManager fm) {
@@ -41,6 +41,6 @@ public class SectionsPagerAdapter extends FragmentPagerAdapter {
     @Override
     public int getCount() {
         // Show 2 total pages.
-        return 2;
+        return TAB_TITLES.length;
     }
 }
