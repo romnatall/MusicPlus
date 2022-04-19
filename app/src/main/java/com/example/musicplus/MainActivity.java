7955643258751fdc9b737b4701e54364fec98777
Роман Наталичев <natalichev.ra@edu.spbstu.ru>
2022-04-19 15:56:34 +0300

    сделал темную тему

--- app/src/main/java/com/example/musicplus/MainActivity.java
+++ app/src/main/java/com/example/musicplus/MainActivity.java
@@ -37,8 +37,6 @@ public class MainActivity extends AppCompatActivity {
         getSupportFragmentManager().beginTransaction()
                 .add(R.id.PlayerFragment, MusicPlayerFragment.class, null)
                 .commit();
-
-
     }
 
 }
